3b6f53ee8a4226c791754bd6dfabd49de6a759ee
kader009 <kadermollarasel000@gmail.com>
2024-12-23 23:55:00 +0600

    use docs for better understand

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 const express = require('express');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 const dotenv = require('dotenv');
 const cors = require('cors');
 const app = express();
 dotenv.config();
 
-// middleware
+/**
+ * Middleware Configuration
+ * - @express.json() for parsing JSON request bodies
+ * - @cors() for enabling Cross-Origin Resource Sharing
+ */
 app.use(express.json());
-app.use(cors());
+app.use(cors()); // this cors is used to connect the frontend and backend
 
 mongoose
   .connect(process.env.DB_URL)
@@ -38,6 +42,58 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 module.exports = User;
 
+// post schema
+const postSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, 'text is needed'],
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Posts = mongoose.model('posts', postSchema);
+module.exports = Posts;
+
+// user post
+app.post('/post', async (req, res) => {
+  try {
+    const posts = new Posts(req.body);
+    await posts.save();
+    res.status(201).send({ message: 'Post create successfully..' });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+// all post
+app.get('/post', async (req, res) => {
+  const post = await Posts.find();
+  res.send(post);
+});
+
+// update post
+app.patch('/post/:id', async (req, res) => {
+  const { id } = req.params;
+  const updateData = req.body;
+
+  try {
+    const updatePost = await Posts.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatePost) {
+      return res.status(404).json({ message: 'post not found' });
+    }
+    res.send(updatePost);
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating post', error });
+  }
+});
+
 // user post
 app.post('/register', async (req, res) => {
   try {
@@ -49,6 +105,7 @@ app.post('/register', async (req, res) => {
   }
 });
 
+// user login
 app.post('/login', async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
@@ -60,6 +117,7 @@ app.post('/login', async (req, res) => {
   res.send({ message: 'Login user successfully.' });
 });
 
+// get all user
 app.get('/user', async (req, res) => {
   const user = await User.find();
   res.send(user);
